e0d25f59d55354f782ae5d4df4a82b91e814690b
sharmakavita0262 <sharmakavita0262@gmail.com>
2023-04-12 08:02:01 +0530

    added host

--- templesite/settings.py
+++ templesite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tt)p2=&*c22t$mu-2=)%#@8vemz6hglmn6um@l9!raktz+m6hc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
